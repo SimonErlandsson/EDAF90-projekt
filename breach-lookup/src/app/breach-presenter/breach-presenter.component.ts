db45c19e07eff28ab5f8fd4d29d6fc4e4e7d2856
Simon Erlandsson <kontakt@simonerlandsson.se>
2022-02-23 16:12:40 +0100

    Merged subscription leak fix to new breach-presenter

--- breach-lookup/src/app/breach-presenter/breach-presenter.component.ts
+++ breach-lookup/src/app/breach-presenter/breach-presenter.component.ts
@@ -1,47 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { BreachService } from '../services/breach.service';
 import { Breach } from '../model/Breach';
 import { Sort } from '@angular/material/sort';
+import { Subscription } from 'rxjs';
 @Component({
-  selector: 'app-breach-presenter',
-  templateUrl: './breach-presenter.component.html',
-  styleUrls: ['./breach-presenter.component.css'],
+    selector: 'app-breach-presenter',
+    templateUrl: './breach-presenter.component.html',
+    styleUrls: ['./breach-presenter.component.css'],
 })
-export class BreachPresenterComponent implements OnInit {
-  breaches: Breach[] = [];
+export class BreachPresenterComponent implements OnInit, OnDestroy {
+    breaches: Breach[] = [];
+    subscription: Subscription;
 
-  constructor(private breachService: BreachService) {}
+    constructor(private breachService: BreachService) { }
 
-  ngOnInit(): void {
-    this.breachService
-      .getBreaches()
-      .subscribe((breaches) => (this.breaches = breaches));
-  }
-  sortData(sort: Sort) {
-    const data = this.breaches.slice();
-    if (!sort.active || sort.direction === '') {
-      this.breaches = data;
-      return;
+    ngOnInit(): void {
+        this.subscription = this.breachService
+            .getBreaches()
+            .subscribe((breaches) => (this.breaches = breaches));
     }
 
-    this.breaches = data.sort((a, b) => {
-      const isAsc = sort.direction === 'asc';
-      switch (sort.active) {
-        case 'name':
-          return compare(a.Name, b.Name, isAsc);
-        case 'pwnCount':
-          return compare(a.PwnCount, b.PwnCount, isAsc);
-        case 'breach_date':
-          return compare(a.BreachDate, b.BreachDate, isAsc);
-        case 'domain':
-          return compare(a.Domain, b.Domain, isAsc);
-        default:
-          return 0;
-      }
-    });
-  }
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe()
+    }
+
+    sortData(sort: Sort) {
+        const data = this.breaches.slice();
+        if (!sort.active || sort.direction === '') {
+            this.breaches = data;
+            return;
+        }
+
+        this.breaches = data.sort((a, b) => {
+            const isAsc = sort.direction === 'asc';
+            switch (sort.active) {
+                case 'name':
+                    return compare(a.Name, b.Name, isAsc);
+                case 'pwnCount':
+                    return compare(a.PwnCount, b.PwnCount, isAsc);
+                case 'breach_date':
+                    return compare(a.BreachDate, b.BreachDate, isAsc);
+                case 'domain':
+                    return compare(a.Domain, b.Domain, isAsc);
+                default:
+                    return 0;
+            }
+        });
+    }
 }
 
 function compare(a: number | string, b: number | string, isAsc: boolean) {
-  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+    return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }

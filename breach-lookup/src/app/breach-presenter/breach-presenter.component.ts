90bb1a55734b6e0d06abe30039f40e884efb8e86
Simon Erlandsson <kontakt@simonerlandsson.se>
2022-02-23 13:38:31 +0100

    Fixed subscription leak

--- breach-lookup/src/app/breach-presenter/breach-presenter.component.ts
+++ breach-lookup/src/app/breach-presenter/breach-presenter.component.ts
@@ -1,20 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { BreachService } from '../services/breach.service';
 import { Breach } from '../model/Breach';
+import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-breach-presenter',
-  templateUrl: './breach-presenter.component.html',
-  styleUrls: ['./breach-presenter.component.css'],
+    selector: 'app-breach-presenter',
+    templateUrl: './breach-presenter.component.html',
+    styleUrls: ['./breach-presenter.component.css'],
 })
-export class BreachPresenterComponent implements OnInit {
-  breaches: Breach[] = [];
+export class BreachPresenterComponent implements OnInit, OnDestroy {
+    breaches: Breach[] = [];
+    subscription: Subscription
 
-  constructor(private breachService: BreachService) {}
 
-  ngOnInit(): void {
-    this.breachService
-      .getBreaches()
-      .subscribe((breaches) => (this.breaches = breaches));
-  }
+    constructor(private breachService: BreachService) { }
+
+    ngOnInit(): void {
+        this.subscription = this.breachService
+            .getBreaches()
+            .subscribe((breaches) => (this.breaches = breaches));
+    }
+
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe()
+    }
 }

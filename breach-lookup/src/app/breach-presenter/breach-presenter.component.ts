2e0b118a273b1834e3f246a3c82a61d8fc87f3d5
Simon Erlandsson <kontakt@simonerlandsson.se>
2022-02-23 13:39:32 +0100

    Forgot semicolon

--- breach-lookup/src/app/breach-presenter/breach-presenter.component.ts
+++ breach-lookup/src/app/breach-presenter/breach-presenter.component.ts
@@ -10,7 +10,7 @@ import { Subscription } from 'rxjs';
 })
 export class BreachPresenterComponent implements OnInit, OnDestroy {
     breaches: Breach[] = [];
-    subscription: Subscription
+    subscription: Subscription;
 
 
     constructor(private breachService: BreachService) { }

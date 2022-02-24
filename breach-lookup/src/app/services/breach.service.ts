5e72e9fdf3259ce5ab22d31af992a2c1cc585952
Adam Forsberg <adamforsberg94@gmail.com>
2022-02-24 07:06:31 +0100

    removed some old commented code

--- breach-lookup/src/app/services/breach.service.ts
+++ breach-lookup/src/app/services/breach.service.ts
@@ -9,49 +9,9 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class BreachService {
   private apiUrl = 'https://haveibeenpwned.com/api/v3/breaches';
 
-  //BREACHES: Breach[] = [
-  //   {
-  //     name: 'testName',
-  //     title: 'testTitle',
-  //     domain: 'testDomain',
-  //     breachDate: '020202',
-  //     addedDate: '010101',
-  //     modifiedDate: '030303',
-  //     pwnCount: 10,
-  //     description: 'breach example',
-  //     dataClasses: [],
-  //     isVerified: true,
-  //     isFabricated: false,
-  //     isSensitive: true,
-  //     isRetired: false,
-  //     isSpamList: false,
-  //     isMalware: false,
-  //     logoPath: 'testlogoPath',
-  //   },
-  //   {
-  //     name: 'testName2',
-  //     title: 'testTitle',
-  //     domain: 'testDomain',
-  //     breachDate: '020202',
-  //     addedDate: '010101',
-  //     modifiedDate: '030303',
-  //     pwnCount: 100,
-  //     description: 'breach example number 2',
-  //     dataClasses: [],
-  //     isVerified: true,
-  //     isFabricated: false,
-  //     isSensitive: true,
-  //     isRetired: false,
-  //     isSpamList: false,
-  //     isMalware: false,
-  //     logoPath: 'testlogoPath',
-  //   },
-  // ];
   constructor(private http: HttpClient) {}
 
   getBreaches(): Observable<Breach[]> {
-    // const breaches = of(this.BREACHES);
-    // return breaches;
-    return this.http.get<Breach[]>(this.apiUrl,{})
+    return this.http.get<Breach[]>(this.apiUrl, {});
   }
 }
